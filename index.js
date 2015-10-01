1ffd0945a336a0be61b12fb96c8be230d664601b
voutilai <jari-pekka.voutilainen@tut.fi>
2015-10-01 15:23:12 +0300

    Module structure

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-module.exports.vdom = {};
+var virtualize = require('vdom-virtualize');
+var patch = require('virtual-dom/patch');
+var diff = require('virtual-dom/diff');
+var Serializer = require('vdom-serialize');
+
+var virtualizedState = null;
+
+
+var Liquid = function(options){
+
+};
+
+
+Liquid.prototype.vdom.virtualize = function(){
+    virtualizedState = virtualize(document.body);
+};
+
+Liquid.prototype.vdom.restore = function(){
+    if ( virtualizedState != null ) {
+        var patches = diff(initialState, virtualizedState);
+        patch(document.body, patches)
+    }
+};
+
+Liquid.prototype.vdom.transfer = function(){
+    var patches = diff(initialState, virtualizedState);
+    var toSync = Serializer.serialize(patches);
+    socket.emit("sync", toSync);
+};
+
+module.exports = Liquid;
