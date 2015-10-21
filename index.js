f7b5b44b01587485c397ee3734dae122aec5bbb9
voutilai <jari-pekka.voutilainen@tut.fi>
2015-10-21 09:36:09 +0300

    Move vdom implementation to its own file

--- index.js
+++ index.js
@@ -1,49 +1,12 @@
-var virtualize = require('vdom-virtualize');
-var patch = require('virtual-dom/patch');
-var diff = require('virtual-dom/diff');
-var Serializer = require('vdom-serialize');
-
-var virtualizedState = null;
-var initialState = null;
+var VDom = require('./lib/VDom.js');
 
 
 var Liquid = function(options){
     this.options = options || {};
-    if (!this.options.transferMethod){
-        this.options.transferMethod = 'socket.io';
-        var io = require('socket.io-client');
-        this.client = io('http://localhost');
-    }
+    this.options.vdom = this.options.vdom || {};
+    this.vdom = new VDom(this.options.vdom);
 
-    initialState = virtualize(document.body);
 };
 
-Liquid.prototype.vdom = {};
-
-Liquid.prototype.vdom.virtualize = function(){
-    virtualizedState = virtualize(document.body);
-};
-
-Liquid.prototype.vdom.restore = function(){
-    if ( virtualizedState != null ) {
-        var patches = diff(initialState, virtualizedState);
-        patch(document.body, patches)
-    }
-};
-
-Liquid.prototype.vdom.transfer = function(){
-    var patches = diff(initialState, virtualizedState);
-    var toSync = Serializer.serialize(patches);
-    if (this.options.transferMethod == 'socket.io'){}
-        this.client.emit("sync", toSync);
-};
-
-
-Liquid.prototype.vdom.restore = function(){
-    if ( virtualizedState != null ) {
-        var patches = diff(initialState, virtualizedState);
-        patch(document.body, patches)
-    }
-};
 
 module.exports = Liquid;
