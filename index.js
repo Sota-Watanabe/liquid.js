84482953f0862f60e8a56b9a58acf7973a22ba95
voutilai <jari-pekka.voutilainen@tut.fi>
2015-10-22 10:17:07 +0300

    initial implementation of ui elements

--- index.js
+++ index.js
@@ -6,7 +6,25 @@ var Liquid = function(options){
     this.options.vdom = this.options.vdom || {};
     this.vdom = new VDom(this.options.vdom);
 
+    this.options.createUI = this.options.createUI || true;
+    if ( this.options.createUI == true ){
+        CreateUIElements();
+    }
+
 };
 
+
+var CreateUIElements = function(){
+    var syncButton = document.createElement('button');
+    syncButton.classList.add('Liquid-ui');
+    syncButton.onclick = sync;
+
+    document.body.appendChild(syncButton)
+};
+
+var sync = function(){
+    this.vdom.virtualize()
+    this.vdom.transfer();
+};
 
 module.exports = Liquid;
